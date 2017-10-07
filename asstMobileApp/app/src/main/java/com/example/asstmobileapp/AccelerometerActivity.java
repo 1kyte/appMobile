c94fd77bd07eeb611e0f0674de1846fc9db9a661
Kate <xunwanwang@gmail.com>
2017-10-07 19:53:00 +1100

    delete all necessary files

--- asstMobileApp/app/src/main/java/com/example/asstmobileapp/AccelerometerActivity.java
+++ asstMobileApp/app/src/main/java/com/example/asstmobileapp/AccelerometerActivity.java
@@ -45,7 +45,7 @@ public class AccelerometerActivity extends AppCompatActivity implements View.OnC
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.step_count);
+        setContentView(R.layout.activity_accelerometer);
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
 
